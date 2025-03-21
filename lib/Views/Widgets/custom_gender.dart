dbd82de68e5ff7fc27348de293ff2702c5068ee9
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-21 12:12:19 +0200

    add font awesome to my app

--- lib/Views/Widgets/custom_gender.dart
+++ lib/Views/Widgets/custom_gender.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CustomGender extends StatelessWidget {
-  const CustomGender({super.key});
+  const CustomGender({super.key, required this.icon});
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,7 +13,7 @@ class CustomGender extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Icons.person, size: 70),
+          Icon(icon, size: 90),
           SizedBox(height: 16),
           Text(
             "Male",
--- lib/Views/Widgets/section_gender.dart
+++ lib/Views/Widgets/section_gender.dart
@@ -1,10 +1,9 @@
 import 'package:bmi_calcultor/Views/Widgets/custom_gender.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ScetionGender extends StatelessWidget {
-  const ScetionGender({
-    super.key,
-  });
+  const ScetionGender({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +11,9 @@ class ScetionGender extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Row(
         children: [
-          Expanded(child: CustomGender()),
+          Expanded(child: CustomGender(icon: FontAwesomeIcons.mars)),
           SizedBox(width: 20),
-          Expanded(child: CustomGender()),
+          Expanded(child: CustomGender(icon: FontAwesomeIcons.venus)),
         ],
       ),
     );
